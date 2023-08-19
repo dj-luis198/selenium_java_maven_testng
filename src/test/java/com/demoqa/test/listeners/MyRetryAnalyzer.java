0eeedaf9d7169547a42c1b76fc046c9085a61569
dj-luis198 <dj.luis198@gmail.com>
2023-08-19 15:35:53 -0300

    add re run and parallet test

---
+++ src/test/java/com/demoqa/test/listeners/MyRetryAnalyzer.java
@@ -0,0 +1,19 @@
+package com.demoqa.test.listeners;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class MyRetryAnalyzer implements IRetryAnalyzer{
+     int counter=0;
+      int retryLimit=1;
+
+    @Override
+    public boolean retry(ITestResult result) {
+     if(counter<retryLimit){
+        counter++;
+        return true;
+     }
+        return false;
+    }
+    
+}
