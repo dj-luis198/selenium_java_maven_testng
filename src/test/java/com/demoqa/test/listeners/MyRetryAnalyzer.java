6419b8869d4e9488eefaf05e7e80dce0756fe21a
dj-luis198 <dj.luis198@gmail.com>
2023-08-29 22:00:00 -0300

    retry limit 3

--- src/test/java/com/demoqa/test/listeners/MyRetryAnalyzer.java
+++ src/test/java/com/demoqa/test/listeners/MyRetryAnalyzer.java
@@ -3,17 +3,22 @@ package com.demoqa.test.listeners;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
-public class MyRetryAnalyzer implements IRetryAnalyzer{
-     int counter=0;
-      int retryLimit=2;
+public class MyRetryAnalyzer implements IRetryAnalyzer {
+   int counter = 0;
+   int retryLimit = 3;
 
-    @Override
-    public boolean retry(ITestResult result) {
-     if(counter<retryLimit){
-        counter++;
-        return true;
-     }
-        return false;
-    }
-    
+   @Override
+   public boolean retry(ITestResult result) {
+      if (counter < retryLimit) {
+         System.out.println("---------------------------------------------------------");
+         System.out.println("fallo de prueba context: " + result.getTestContext());
+         System.out.println("fallo de prueba: " + result.getMethod().getMethodName());
+         counter++;
+         System.out.println("intento no: " + counter + " de 3");
+         System.out.println("----------------------------------------------------------");
+         return true;
+      }
+      return false;
+   }
+
 }
