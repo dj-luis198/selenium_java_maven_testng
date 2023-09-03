757e7af3ec55d5fe1d46e1296db575c552b5ce41
dj-luis198 <dj.luis198@gmail.com>
2023-09-03 12:52:50 -0300

    test9

--- src/main/java/com/demoqa/pages/common/AdsFooter.java
+++ src/main/java/com/demoqa/pages/common/AdsFooter.java
@@ -5,7 +5,7 @@ import com.demoqa.base.BaseClass;
 public class AdsFooter extends BaseClass {
 
     private final String ads = "//div[@id='fixedban']";
-    private final String footer ="//div[@id='app']//footer";
+    private final String footer ="//footer";
     private final String sidebar ="//div[contains(@class,'sidebar')]";
 
 
