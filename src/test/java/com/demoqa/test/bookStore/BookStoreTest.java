66c0bcd539a3e27b8d480c37ca084f688a76e02f
dj-luis198 <dj.luis198@gmail.com>
2023-09-22 11:32:02 -0300

    BooksStore

--- src/test/java/com/demoqa/test/bookStore/BookStoreTest.java
+++ src/test/java/com/demoqa/test/bookStore/BookStoreTest.java
@@ -6,14 +6,14 @@ import java.util.List;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import com.demoqa.base.BaseTest;
+import com.demoqa.base.BaseTestMethod;
 import com.demoqa.pages.bookStore.BooksStorePage;
 import com.demoqa.pages.bookStore.LoginStorePage;
 import com.demoqa.pages.bookStore.ProfileStorePage;
 import com.demoqa.pages.common.AdsFooter;
 import com.demoqa.util.dataProvider.bookStoreE2EData;
 
-public class BookStoreTest extends BaseTest {
+public class BookStoreTest extends BaseTestMethod {
     BooksStorePage booksStore = new BooksStorePage();
     LoginStorePage loginStorePage = new LoginStorePage();
     ProfileStorePage profileStorePage = new ProfileStorePage();
