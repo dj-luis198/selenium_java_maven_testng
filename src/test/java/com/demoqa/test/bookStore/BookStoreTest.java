03f170ee44e1224d59c340d7e752b86a5c1c606e
dj-luis198 <dj.luis198@gmail.com>
2023-09-23 16:39:32 -0300

    test books store

--- src/test/java/com/demoqa/test/bookStore/BookStoreTest.java
+++ src/test/java/com/demoqa/test/bookStore/BookStoreTest.java
@@ -21,49 +21,75 @@ public class BookStoreTest extends BaseTest{
 
     @BeforeMethod
     public void preconditions(){
+        System.out.println("comienzan precondiciones");
         booksStore = new BooksStorePage();
         loginStorePage = new LoginStorePage();
         profileStorePage = new ProfileStorePage();
         homePage= new HomePage();
         adsFooter= new AdsFooter();
         homePage.goToLoginPage();
+        System.out.println("go to login page");
         adsFooter.deleteAds();
+        System.out.println("delete ads");
     }
 
     @Test(dataProvider = "bookStoreE2E", dataProviderClass = bookStoreE2EData.class)
     public void booksStoreE2ETest(String userName, String pass, String books) {
         loginStorePage.LoginUser(userName, pass);
+        System.out.println("ingrese datos de login");
         adsFooter.deleteAds();
+        System.out.println("delete ads");
         Assert.assertEquals(profileStorePage.returnUserName(), userName);
         profileStorePage.clickGoToBookStoreButton();
+        System.out.println("ir a book store");
         adsFooter.deleteAds();
+        System.out.println("delete ads");
         Assert.assertEquals(booksStore.returnUserName(), userName);
         booksStore.selectBooks(books);
+        System.out.println("selecciona los libros");
         booksStore.clickProfile();
+        System.out.println("ir a profile");
         adsFooter.deleteAds();
+        System.out.println("delete ads");
         Assert.assertTrue(profileStorePage.verifyBooksList(books));
         profileStorePage.deleteBooks(books);
+        System.out.println("elimina libros");
         Assert.assertTrue(profileStorePage.verifyBooksDelete(books));
         profileStorePage.clickGoToBookStoreButton();
+        System.out.println("ir a bookstore por segunda vez");
         adsFooter.deleteAds();
+        System.out.println("delete ads");
         booksStore.selectBooks(books);
+        System.out.println("select segunda camada de libros");
         booksStore.clickProfile();
+        System.out.println("ir a profile");
         adsFooter.deleteAds();
+        System.out.println("delete ads");
         profileStorePage.clickDeleteAllBooks();
+        System.out.println("elimino todos los libros");
         profileStorePage.confirmSmallModal();
+        System.out.println("confirma modal");
         profileStorePage.acceptAlertDeleteAll();
+        System.out.println("asepto alerta");
         Assert.assertTrue(profileStorePage.verifyDeleteAllBooks());
     }
 
     @AfterMethod
     public void deleteBooks(){
         booksStore.clickProfile();
+        System.out.println("after comienza... voy a profile");
         adsFooter.deleteAds();
+        System.out.println("delete ads");
         profileStorePage.clickDeleteAllBooks();
+        System.out.println("delete todos los libros");
         profileStorePage.confirmSmallModal();
+        System.out.println("confirm modal");
         profileStorePage.acceptAlertDeleteAll();
+        System.out.println("confirm alert");
         profileStorePage.clickLogOut();
+        System.out.println("logout");
         homePage.goToHome();
-        adsFooter.deleteAds();
+        System.out.println("ir a home");
+        System.out.println("delete ads y fin after.......");
     }
 }
