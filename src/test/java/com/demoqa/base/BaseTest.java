cb9b8a4c27f1851236a6a0bfc82da31f6ec04a9d
dj-luis198 <dj.luis198@gmail.com>
2023-09-24 19:23:53 -0300

    firefox error colors

--- src/test/java/com/demoqa/base/BaseTest.java
+++ src/test/java/com/demoqa/base/BaseTest.java
@@ -2,6 +2,7 @@ package com.demoqa.base;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 public class BaseTest extends BaseClass {
@@ -9,7 +10,7 @@ public class BaseTest extends BaseClass {
     
     @BeforeClass
     @Parameters(value = {"browser"})
-    public void setUp(String browser) {
+    public void setUp(@Optional("chrome")String browser) {
         init(browser);
     }
 
