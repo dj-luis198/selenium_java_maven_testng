292a25c9e7d3da8e003154a55d7c2620e617db43
dj-luis198 <dj.luis198@gmail.com>
2023-12-09 14:37:20 -0300

    fefactor config and xml test

--- src/test/java/com/demoqa/util/listeners/MyReRunConfig.java
+++ src/test/java/com/demoqa/util/listeners/MyReRunConfig.java
@@ -17,12 +17,6 @@ public class MyReRunConfig implements IConfigurable {
                 callBack.runConfigurationMethod(testResult);
                 if (testResult.getThrowable() == null) {
                     break;
-                }else {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
                 }
             }
         }
