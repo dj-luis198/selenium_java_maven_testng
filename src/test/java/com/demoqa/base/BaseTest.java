1316d2b1cfb0180b7423289b4871722638a1fd36
dj-luis198 <dj.luis198@gmail.com>
2024-01-01 21:12:21 -0300

    cambio de init class a method

--- src/test/java/com/demoqa/base/BaseTest.java
+++ src/test/java/com/demoqa/base/BaseTest.java
@@ -2,15 +2,15 @@ package com.demoqa.base;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 public class BaseTest extends BaseClass {
     private static Logger logger= LogManager.getLogger(BaseTest.class);
 
-    @BeforeClass
+    @BeforeMethod
     @Parameters(value = { "browser" })
     public void setUp(@Optional("chrome") String browser) {
         int maxRetries = 3;
@@ -32,7 +32,7 @@ public class BaseTest extends BaseClass {
         }
     }
 
-    @AfterClass
+    @AfterMethod
     public void tearDown() {
         quit();
     }
